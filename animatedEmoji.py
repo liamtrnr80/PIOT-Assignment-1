609062087646b22dea8583b8e9fd4d1a3435defb
Liam <liamtrnr80@gmail.com>
2020-03-10 10:40:19 +1100

    Updated animatedEmoji.py
    90% finished at this point

--- animatedEmoji.py
+++ animatedEmoji.py
@@ -6,22 +6,55 @@ sense = SenseHat()
 w = (150, 150, 150)
 b = (142, 71, 0)
 bro = (204, 102, 0)
+y = (255, 192, 0)
 e = (0, 0, 0)
+p = (150,0,150)
 
-image = [
-    e,e,e,e,b,e,e,e,
-    e,e,e,bro,bro,e,e,e,
-    e,e,b,b,b,b,e,e,
-    e,bro,w,bro,w,bro,e,e,
-    e,b,b,b,b,b,b,e,
-    bro,bro,w,w,w,w,bro,bro,
-    b,b,b,b,b,b,b,b,
-    e,e,e,e,e,e,e,e
+emoji = [
+    [
+        w,w,w,w,b,w,w,w,
+        w,w,w,b,b,w,w,w,
+        w,w,b,b,b,b,w,w,
+        w,b,e,b,e,b,w,w,
+        w,b,b,b,b,b,b,w,
+        b,b,e,e,e,e,b,b,
+        b,b,b,b,b,b,b,b,
+        w,w,w,w,w,w,w,w
+    ],
+    [
+        w,y,y,y,y,y,y,w,
+        y,y,y,y,y,y,y,y,
+        y,e,e,y,y,e,e,y,
+        y,e,e,y,y,e,e,y,
+        y,y,y,y,y,y,y,y,
+        y,e,y,y,y,y,e,y,
+        y,y,e,e,e,e,y,y,
+        w,y,y,y,y,y,y,w
+    ],
+    [
+        w,p,p,p,p,p,p,w,
+        p,p,p,p,p,p,p,p,
+        p,e,p,p,p,p,e,p,
+        p,e,e,p,p,e,e,p,
+        p,p,p,p,p,p,p,p,
+        p,p,p,p,p,p,e,p,
+        p,p,e,e,e,e,p,p,
+        w,p,p,p,p,p,p,w
+    ]
 ]
 
-sense.set_pixels(image)
+n = 0
 
 while True:
-    sleep(30)
-    sense.clear()
-    
+    if n == 0:
+        sense.set_pixels(emoji[n])
+        n = 1
+        sleep(1)
+    elif n == 1:
+        sense.set_pixels(emoji[n])
+        n = 2
+        sleep(1)
+    elif n == 2:
+        sense.set_pixels(emoji[n])
+        n = 0
+        sleep(1)
