7b98717968cb49510d1c86e8b4834e419c874d45
Liam <liamtrnr80@gmail.com>
2020-03-13 14:16:20 +1100

    Update electronicDie.py

--- electronicDie.py
+++ electronicDie.py
@@ -14,71 +14,71 @@ dice = [
         w,w,w,w,w,w,w,w,
         w,w,w,w,w,w,w,w,
         w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w
-    ],
-    [
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
+        w,w,w,b,b,w,w,w,
+        w,w,w,b,b,w,w,w,
         w,w,w,w,w,w,w,w,
         w,w,w,w,w,w,w,w,
         w,w,w,w,w,w,w,w
     ],
     [
         w,w,w,w,w,w,w,w,
+        w,b,b,w,w,w,w,w,
+        w,b,b,w,w,w,w,w,
         w,w,w,w,w,w,w,w,
         w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
+        w,w,w,w,w,b,b,w,
+        w,w,w,w,w,b,b,w,
         w,w,w,w,w,w,w,w
     ],
     [
         w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
+        w,b,b,w,w,w,w,w,
+        w,b,b,w,w,w,w,w,
+        w,w,w,b,b,w,w,w,
+        w,w,w,b,b,w,w,w,
+        w,w,w,w,w,b,b,w,
+        w,w,w,w,w,b,b,w,
         w,w,w,w,w,w,w,w
     ],
     [
         w,w,w,w,w,w,w,w,
+        w,b,b,w,w,b,b,w,
+        w,b,b,w,w,b,b,w,
         w,w,w,w,w,w,w,w,
         w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
+        w,b,b,w,w,b,b,w,
+        w,b,b,w,w,b,b,w,
         w,w,w,w,w,w,w,w
     ],
     [
         w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
+        w,b,b,w,w,b,b,w,
+        w,b,b,w,w,b,b,w,
+        w,w,w,b,b,w,w,w,
+        w,w,w,b,b,w,w,w,
+        w,b,b,w,w,b,b,w,
+        w,b,b,w,w,b,b,w,
         w,w,w,w,w,w,w,w
+    ],
+    [
+        w,b,b,w,w,b,b,w,
+        w,b,b,w,w,b,b,w,
+        w,w,w,w,w,w,w,w,
+        w,b,b,w,w,b,b,w,
+        w,b,b,w,w,b,b,w,
+        w,w,w,w,w,w,w,w,
+        w,b,b,w,w,b,b,w,
+        w,b,b,w,w,b,b,w
     ]
 ]
 
 def dice_roll():
     n = random.randint(3, 9)
     while n != 0:
-        sense.set_pixel(random.choice(dice))
+        sense.set_pixels(random.choice(dice))
         n -= 1
         sleep(.2)
-        
+
 
 while True:
     x, y, z = sense.get_accelerometer_raw().values()
@@ -88,5 +88,6 @@ while True:
     z = abs(z)
 
     if x > 2 or y > 2 or z > 2:
-        dice_roll()
+        sense.set_pixels(random.choice(dice))
         sleep(2)
+        sense.clear()
