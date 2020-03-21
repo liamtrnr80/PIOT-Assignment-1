248e94e8748b30d6a43817317ca251984156019a
Joseph Williams <s3569541@studennt.rmit.edu.au>
2020-03-21 14:53:37 +1100

    started on the game logic

---
+++ game.py
@@ -0,0 +1,36 @@
+from sense_hat import SenseHat
+from time import sleep
+import random
+from electronicDie import RollDice
+
+class TwoPlayerGame(self, player1, player2):
+    player1_score = 0
+    player2_score = 0
+
+    def __init__(self):
+        self.player1 = player1
+        self.player2 = player2
+
+
+
+def main():
+
+    rollDice = RollDice()
+    rollDice.sense.clear()
+    
+    while True:
+        x, y, z = rollDice.sense.get_accelerometer_raw().values()
+
+        x = abs(x)
+        y = abs(y)
+        z = abs(z)
+
+        if x > 2 or y > 2 or z > 2:
+            rollDice.dice_roll()
+            sleep(2)
+            rollDice.sense.clear()
+        
+
+
+if __name__ == '__main__':
+    main()
