f1f1c9cafe64b6cba9cea532feb3d373c6d77bbc
Joseph Williams <s3569541@studennt.rmit.edu.au>
2020-03-21 13:56:45 +1100

    further formatting

--- electronicDie.py
+++ electronicDie.py
@@ -81,17 +81,18 @@ class RollDice:
 
 def main():
     sense = SenseHat()
-    rollDice = RollDice()
+    sense.set_pixels(0,3,55)
+    # rollDice = RollDice()
 
-    sense.clear()
-    while True:
-        x, y, z = sense.get_accelerometer_raw().values()
+    # sense.clear()
+    # while True:
+    #     x, y, z = sense.get_accelerometer_raw().values()
 
-        x = abs(x)
-        y = abs(y)
-        z = abs(z)
+    #     x = abs(x)
+    #     y = abs(y)
+    #     z = abs(z)
 
-        if x > 2 or y > 2 or z > 2:
-            sense.set_pixels(random.choice(rollDice.dice))
-            sleep(2)
-            sense.clear()
+    #     if x > 2 or y > 2 or z > 2:
+    #         sense.set_pixels(random.choice(rollDice.dice))
+    #         sleep(2)
+    #         sense.clear()
