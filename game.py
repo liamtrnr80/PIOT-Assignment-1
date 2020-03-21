a707434cfdc0516a8640e9d18e4c7bf94f00d0b8
Joseph Williams <s3569541@studennt.rmit.edu.au>
2020-03-21 15:21:53 +1100

    setting up point scoring

--- game.py
+++ game.py
@@ -27,8 +27,7 @@ def main():
         z = abs(z)
 
         if x > 2 or y > 2 or z > 2:
-            rollDice.dice_roll
-            print("Shaking")
+            rollDice.dice_roll()
             # print("Value Returned:", rollDice.get_dice_roll())
             sleep(2)
             rollDice.sense.clear()
