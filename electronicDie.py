3c35646debe78483cc46ddbda43e0ae5ca0600fa
Joseph Williams <s3569541@studennt.rmit.edu.au>
2020-03-21 13:54:43 +1100

    further formatting

--- electronicDie.py
+++ electronicDie.py
@@ -3,7 +3,7 @@ from time import sleep
 import random
 
 class RollDice:
-    sense = SenseHat()
+    
     b = [0, 0, 0]
     w = [150, 150, 150]
     n = random.randrange(0,5)
@@ -80,17 +80,18 @@ class RollDice:
             sleep(.2)
 
 def main():
+    sense = SenseHat()
     rollDice = RollDice()
 
-    rollDice.sense.clear()
+    sense.clear()
     while True:
-        x, y, z = rollDice.sense.get_accelerometer_raw().values()
+        x, y, z = sense.get_accelerometer_raw().values()
 
         x = abs(x)
         y = abs(y)
         z = abs(z)
 
         if x > 2 or y > 2 or z > 2:
-            rollDice.sense.set_pixels(random.choice(rollDice.dice))
+            sense.set_pixels(random.choice(rollDice.dice))
             sleep(2)
-            rollDice.sense.clear()
+            sense.clear()
