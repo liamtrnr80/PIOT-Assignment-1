1d5cb988b1d29a8db1893048aa7db2a0ca74e715
Joseph Williams <s3569541@studennt.rmit.edu.au>
2020-03-21 13:42:56 +1100

    reformatted into a class

--- electronicDie.py
+++ electronicDie.py
@@ -2,92 +2,96 @@ from sense_hat import SenseHat
 from time import sleep
 import random
 
-sense = SenseHat()
+class RollDice:
+    sense = SenseHat()
+    b = [0, 0, 0]
+    w = [150, 150, 150]
+    n = random.randrange(0,5)
 
-sense.clear()
+    def __init__(self):
+        self.dice = [
+            [
+                w,w,w,w,w,w,w,w,
+                w,w,w,w,w,w,w,w,
+                w,w,w,w,w,w,w,w,
+                w,w,w,b,b,w,w,w,
+                w,w,w,b,b,w,w,w,
+                w,w,w,w,w,w,w,w,
+                w,w,w,w,w,w,w,w,
+                w,w,w,w,w,w,w,w
+            ],
+            [
+                w,w,w,w,w,w,w,w,
+                w,b,b,w,w,w,w,w,
+                w,b,b,w,w,w,w,w,
+                w,w,w,w,w,w,w,w,
+                w,w,w,w,w,w,w,w,
+                w,w,w,w,w,b,b,w,
+                w,w,w,w,w,b,b,w,
+                w,w,w,w,w,w,w,w
+            ],
+            [
+                w,w,w,w,w,w,w,w,
+                w,b,b,w,w,w,w,w,
+                w,b,b,w,w,w,w,w,
+                w,w,w,b,b,w,w,w,
+                w,w,w,b,b,w,w,w,
+                w,w,w,w,w,b,b,w,
+                w,w,w,w,w,b,b,w,
+                w,w,w,w,w,w,w,w
+            ],
+            [
+                w,w,w,w,w,w,w,w,
+                w,b,b,w,w,b,b,w,
+                w,b,b,w,w,b,b,w,
+                w,w,w,w,w,w,w,w,
+                w,w,w,w,w,w,w,w,
+                w,b,b,w,w,b,b,w,
+                w,b,b,w,w,b,b,w,
+                w,w,w,w,w,w,w,w
+            ],
+            [
+                w,w,w,w,w,w,w,w,
+                w,b,b,w,w,b,b,w,
+                w,b,b,w,w,b,b,w,
+                w,w,w,b,b,w,w,w,
+                w,w,w,b,b,w,w,w,
+                w,b,b,w,w,b,b,w,
+                w,b,b,w,w,b,b,w,
+                w,w,w,w,w,w,w,w
+            ],
+            [
+                w,b,b,w,w,b,b,w,
+                w,b,b,w,w,b,b,w,
+                w,w,w,w,w,w,w,w,
+                w,b,b,w,w,b,b,w,
+                w,b,b,w,w,b,b,w,
+                w,w,w,w,w,w,w,w,
+                w,b,b,w,w,b,b,w,
+                w,b,b,w,w,b,b,w
+            ]
+        ]
+        
+    def dice_roll(self):
+        n = random.randint(3, 9)
+        # (random.randrange(1,10))
+        while n != 0:
+            self.sense.set_pixels(random.choice(dice))
+            n -= 1
+            sleep(.2)
 
-b = [0, 0, 0]
-w = [150, 150, 150]
+def main():
+    rollDice = RollDice()
 
-dice = [
-    [
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,b,b,w,w,w,
-        w,w,w,b,b,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w
-    ],
-    [
-        w,w,w,w,w,w,w,w,
-        w,b,b,w,w,w,w,w,
-        w,b,b,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,b,b,w,
-        w,w,w,w,w,b,b,w,
-        w,w,w,w,w,w,w,w
-    ],
-    [
-        w,w,w,w,w,w,w,w,
-        w,b,b,w,w,w,w,w,
-        w,b,b,w,w,w,w,w,
-        w,w,w,b,b,w,w,w,
-        w,w,w,b,b,w,w,w,
-        w,w,w,w,w,b,b,w,
-        w,w,w,w,w,b,b,w,
-        w,w,w,w,w,w,w,w
-    ],
-    [
-        w,w,w,w,w,w,w,w,
-        w,b,b,w,w,b,b,w,
-        w,b,b,w,w,b,b,w,
-        w,w,w,w,w,w,w,w,
-        w,w,w,w,w,w,w,w,
-        w,b,b,w,w,b,b,w,
-        w,b,b,w,w,b,b,w,
-        w,w,w,w,w,w,w,w
-    ],
-    [
-        w,w,w,w,w,w,w,w,
-        w,b,b,w,w,b,b,w,
-        w,b,b,w,w,b,b,w,
-        w,w,w,b,b,w,w,w,
-        w,w,w,b,b,w,w,w,
-        w,b,b,w,w,b,b,w,
-        w,b,b,w,w,b,b,w,
-        w,w,w,w,w,w,w,w
-    ],
-    [
-        w,b,b,w,w,b,b,w,
-        w,b,b,w,w,b,b,w,
-        w,w,w,w,w,w,w,w,
-        w,b,b,w,w,b,b,w,
-        w,b,b,w,w,b,b,w,
-        w,w,w,w,w,w,w,w,
-        w,b,b,w,w,b,b,w,
-        w,b,b,w,w,b,b,w
-    ]
-]
+    rollDice.sense.clear()
+    while True:
+        x, y, z = rollDice.sense.get_accelerometer_raw().values()
 
-def dice_roll():
-    n = random.randint(3, 9)
-    while n != 0:
-        sense.set_pixels(random.choice(dice))
-        n -= 1
-        sleep(.2)
+        x = abs(x)
+        y = abs(y)
+        z = abs(z)
 
-
-while True:
-    x, y, z = sense.get_accelerometer_raw().values()
-
-    x = abs(x)
-    y = abs(y)
-    z = abs(z)
-
-    if x > 2 or y > 2 or z > 2:
-        sense.set_pixels(random.choice(dice))
-        sleep(2)
-        sense.clear()
+        if x > 2 or y > 2 or z > 2:
+            rollDice.sense.set_pixels(random.choice(rollDice.dice))
+            sleep(2)
+            rollDice.sense.clear()
